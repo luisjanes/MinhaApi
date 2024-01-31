a54d3034cce54ac8457a801830ba9744276e6735
Luis Felipe da Costa Janes <luisf.janes@gmail.com>
2024-01-31 14:55:55 -0300

    feat: add get and post

--- MinhaApi/Program.cs
+++ MinhaApi/Program.cs
@@ -1,6 +1,34 @@
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World!!");
+app.MapGet("/hello", Hello);
+app.MapGet("/hello{name}", HelloName);
+app.MapPost("/", (User user) =>
+{
+    return Results.Ok(user);
+});
+
+
+app.MapGet("/user{id}", () =>
+{
+    return Results.Ok("User Luis Felipe");
+});
 
 app.Run();
+
+string HelloName(string name)
+{
+    return $"User {name}";
+}
+string Hello()
+{
+    return "Hello world with separetade function";
+}
+
+public class User 
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+}
